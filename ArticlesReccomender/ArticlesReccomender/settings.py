13b27e5ab871f7cb48342a4538474e656eefde2f
tstasyuk <tstasyuk@ozon.ru>
2023-02-14 08:37:53 +0300

    fix

--- ArticlesReccomender/ArticlesReccomender/settings.py
+++ ArticlesReccomender/ArticlesReccomender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1*$+r#h7$bud9d^8+jruqpei$3k*t^34f8=egq+uad3gw&fgpt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
